3f233eeb86adad92c3dc235071502b0befd6f365
Abdirahman Suldaan <suldaan.89@hotmail.com>
2022-06-15 04:01:39 +0300

    colorful grid

--- noisyGrid/sketch.js
+++ noisyGrid/sketch.js
@@ -14,10 +14,16 @@ function draw() {
 function colorGrid() {
   // your code here
   push()
-  stroke(0)
-  fill(255)
+  // stroke(0)
+  let green = color('green')
+  let red = color('red')
   for (i = 0; i < 25; i++) {
     for (j = 0; j < 25; j++) {
+      let n = noise(i / 50, j / 50, frameCount / 100)
+      let interA = lerpColor(green, red, n)
+
+      fill(interA)
+      noStroke()
       rect(i * stepSize, j * stepSize, stepSize, stepSize)
     }
   }
