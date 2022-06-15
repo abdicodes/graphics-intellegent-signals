832de87290db1fefb3c3d52dc6a8da4cd878ef0c
Abdirahman Suldaan <suldaan.89@hotmail.com>
2022-06-15 14:29:53 +0300

    compass grid implemented

--- noisyGrid/sketch.js
+++ noisyGrid/sketch.js
@@ -19,10 +19,12 @@ function colorGrid() {
   let red = color('red')
   for (i = 0; i < 25; i++) {
     for (j = 0; j < 25; j++) {
-      let n = noise(i / 50, j / 50, frameCount / 100)
-      let interA = lerpColor(green, red, n)
+      let mouse_pos = map(mouseX, 0, width, 0, 100)
+      let speed = (frameCount * mouse_pos) / 1000
 
-      fill(interA)
+      let n = noise(i / 50, j / 50, speed)
+      let interCol = lerpColor(green, red, n)
+      fill(interCol)
       noStroke()
       rect(i * stepSize, j * stepSize, stepSize, stepSize)
     }
@@ -32,4 +34,22 @@ function colorGrid() {
 ///////////////////////////////////////////////////////////////////////
 function compassGrid() {
   // your code here
+  for (i = 0; i < 25; i++) {
+    for (j = 0; j < 25; j++) {
+      push()
+      translate(stepSize / 2, 0)
+      let n = noise(i / 50, j / 50, frameCount / 500)
+      let c = map(n, 0, 1, 0, 720)
+
+      push()
+      angleMode(DEGREES)
+      translate(i * stepSize, j * stepSize)
+
+      rotate(c)
+      fill(0)
+      line(0, 0, 0, stepSize)
+      pop()
+      pop()
+    }
+  }
 }
