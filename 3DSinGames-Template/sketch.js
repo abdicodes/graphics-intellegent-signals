1b3061d3ebca727e23b82e267aad404396a47a1b
Abdirahman Suldaan <suldaan.89@hotmail.com>
2022-08-12 21:45:14 +0300

    Step1: init boxes and camera pos

--- 3DSinGames-Template/sketch.js
+++ 3DSinGames-Template/sketch.js
@@ -1,9 +1,18 @@
 function setup() {
-    createCanvas(900, 800, WEBGL);
-
+  createCanvas(900, 800, WEBGL);
 }
 
 function draw() {
-    background(125);
-    angleMode(DEGREES);
+  background(125);
+  angleMode(DEGREES);
+  camera(800, -600, 800, 0, 0, 0, 0, 1, 0);
+  fill(255);
+  for (var i = -400; i < 400; i += 50) {
+    for (var j = -400; j < 400; j += 50) {
+      push();
+      translate(i, 0, j);
+      box(50, 50, 50);
+      pop();
+    }
+  }
 }
