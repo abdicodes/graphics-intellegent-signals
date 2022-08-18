cdd59500cce95ae0ebd9287cb2f163fe93ff2e6a
Abdirahman Suldaan <suldaan.89@hotmail.com>
2022-08-18 18:02:33 +0300

    step5: red photo added to the right pixels updated

--- averageFace-Template/sketch.js
+++ averageFace-Template/sketch.js
@@ -9,26 +9,29 @@ function preload() {
     const img = loadImage(`./assets/${i}.jpg`);
     imgs.push(img);
   }
-  avgImg = createGraphics(imgs[0].width, imgs[0].height);
 }
 //////////////////////////////////////////////////////////
 function setup() {
-  createCanvas(imgs[0].width * 2, imgs[0].height);
   pixelDensity(1);
+  createCanvas(imgs[0].width * 2, imgs[0].height);
+  avgImg = createGraphics(imgs[0].width, imgs[0].height);
 }
 //////////////////////////////////////////////////////////
 function draw() {
   background(125);
+
   image(imgs[0], 0, 0);
 
   avgImg.loadPixels();
   imgs.forEach((img) => img.loadPixels());
-  console.log(imgs);
 
-  for (var i = 0; i < height; i++) {
-    for (var j = 0; j < width; j++) {
-      avgImg.pixels[(width * i + j) * 4] = 100;
+  for (let i = 0; i < avgImg.height; i++) {
+    for (let j = 0; j < imgs[0].width; j++) {
+      avgImg.pixels[(avgImg.width * i + j) * 4] = 255;
+      avgImg.pixels[(avgImg.width * i + j) * 4 + 3] = 255;
     }
   }
+  avgImg.updatePixels();
+  image(avgImg, imgs[0].width, 0);
   noLoop();
 }
